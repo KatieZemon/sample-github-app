d8f7fbdeee2ade5ff17ee176fa1d17e27e0dcab6
Katie Zemon <katiezemon@gmail.com>
2023-05-23 10:16:17 -0500

    Added Example code for bot to apply branch protection rules

--- hello_gidgethub.py
+++ hello_gidgethub.py
@@ -6,6 +6,7 @@ import aiohttp
 from aiohttp import web
 from gidgethub import routing
 from gidgethub import sansio
+import gidgethub
 from gidgethub.aiohttp import GitHubAPI
 
 import jwt_generator
@@ -14,7 +15,7 @@ router = routing.Router()
 
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
-SNOW_GATE_PASS = True
+SNOW_GATE_PASS = False
 
 
 async def list_commands(event: sansio.Event):
@@ -158,7 +159,7 @@ async def run_twistlock_gate(repo_full_name: str, installation_id: int, sha: str
     LOGGER.debug("Running Twistlock Scan...")
     await asyncio.sleep(4)  # Pretend twistlock scan took a number of seconds
 
-    twistlock_passed = random.choices([True, False], weights=[0.2, 0.8])[0]
+    twistlock_passed = random.choices([True, False], weights=[0.9, 0.1])[0]
 
     if twistlock_passed:
         LOGGER.debug("Twistlock Passed")
@@ -331,6 +332,77 @@ async def opened_pr(event: sansio.Event):
     required_checks = await get_required_status_checks(repo_full_name, branch, install_id)
 
 
+async def get_branch_protection(repo_full_name: str, installation_id: int):
+    url = f"/repos/{repo_full_name}/branches/main/protection"
+    async with aiohttp.ClientSession() as session:
+        gh = GitHubAPI(session, "katiesamplebot")
+        access_token = await produce_access_token(gh, installation_id)
+        try:
+            response = await gh.getitem(
+                url,
+                oauth_token=access_token
+            )
+        except gidgethub.BadRequest as e:
+            LOGGER.debug("Main Branch is not protected. Need to add a main branch protection")
+            return None
+
+    return response
+
+
+async def update_branch_protection(repo_full_name: str, installation_id: int):
+    url = f"/repos/{repo_full_name}/branches/main/protection"
+    async with aiohttp.ClientSession() as session:
+        gh = GitHubAPI(session, "katiesamplebot")
+        access_token = await produce_access_token(gh, installation_id)
+        response = await gh.put(
+            url,
+            oauth_token=access_token,
+            data = {
+                'required_status_checks': {
+                    'strict': True,
+                    'contexts': [
+                        'ServiceNow Check',
+                        'Twistlock Check'
+                    ]
+                },
+                'enforce_admins': True,
+                'restrictions': None,
+                'required_pull_request_reviews': {
+                    'required_approving_review_count': 1
+                }
+            }
+        )
+    return response
+
+
+def is_branch_protection_valid(response):
+    is_valid = False
+    if 'required_pull_request_reviews' in response and 'required_status_checks' in response:
+        checks = response['required_status_checks']['checks']
+        if len(checks) >= 2 and \
+            any(check['context'] == 'ServiceNow Check' for check in checks) and \
+            any(check['context'] == 'Twistlock Check' for check in checks) and \
+            response['required_pull_request_reviews']['required_approving_review_count'] >= 1:
+            is_valid = True
+    return is_valid
+
+
+# If someone modifies the branch protection rule or removes a required check, add it back
+@router.register(event_type="branch_protection_rule", action="deleted")
+@router.register(event_type="branch_protection_rule", action="edited")
+@router.register(event_type="branch_protection_rule", action="created")
+async def handle_branch_protection_rule_event(event: sansio.Event):
+    repo_full_name = event.data['repository']['full_name']
+    installation_id = event.data['installation']['id']
+    branch_protection = await get_branch_protection(repo_full_name, installation_id)
+    if branch_protection is None:
+        LOGGER.debug("There is no branch protection on main")
+        # TBD create new branch protection
+    elif is_branch_protection_valid(branch_protection):
+        LOGGER.debug("Branch protection looks good!")
+    else:
+        LOGGER.debug("Branch protection is invalid. Updating it")
+        await update_branch_protection(repo_full_name, installation_id)
 
 
 async def gh_event_handler(request: aiohttp.web.Request):
